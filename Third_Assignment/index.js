a6da69dcba32e0904a180fe0635b0cec84231c43
ShahzadTabassum <www.shahzadtabassum786@gmail.com>
2023-08-18 00:57:19 +0500

    Third Assignment solve

---
+++ Third_Assignment/index.js
@@ -0,0 +1,24 @@
+// Write a program to convert the temperature from Celsius to Fahrenheit and vice verse.
+function CtoF_Temperature(Celsius_temp) {
+    var fahrenheit_temp = (Celsius_temp * 9 / 5) + 32;
+    console.log("Temperature in Fahrenheit is", fahrenheit_temp);
+}
+function FtoC_Temperature(Fahrenheit_temp) {
+    var Celsius_temp = (Fahrenheit_temp - 32) * 5 / 9;
+    console.log("Temperature in Celsius is", Celsius_temp);
+}
+CtoF_Temperature(32);
+FtoC_Temperature(80);
+// Write a program that calculates the percentage.
+function percentage(Obtaining_marks, Total_marks) {
+    var percentage = (Obtaining_marks / Total_marks) * 100;
+    console.log("Your Percentage", percentage);
+}
+percentage(721, 1100);
+// Write a program that converts given number of days in to weeks and days such as 17 days = 2 weeks and 3 days.
+function convert_weaks_and_days(days) {
+    var Weaks = Math.floor(days / 7);
+    var day = days % 7;
+    console.log("Number of Weaks", Weaks, "And Days", day);
+}
+convert_weaks_and_days(29);
