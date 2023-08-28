a6606d9f9ecac031d2f2b908bb2942922a91aeba
ShahzadTabassum <www.shahzadtabassum786@gmail.com>
2023-08-28 14:11:51 +0500

    solve some problems in 5th assignment

---
+++ 5th_Assignment/index.ts
@@ -0,0 +1,35 @@
+// - Develop a program that calculates and prints the sum of the first n even numbers using a for loop.
+
+var numbers_array:number[] = [2,2,3,5,6,8,9,10];
+let even_sum = 0
+for (let index = 0; index < numbers_array.length; index++ ){
+    if (numbers_array[index]%2==0){
+         even_sum += numbers_array[index];
+    }
+
+}
+console.log('Sum of Evin Numbers is',even_sum);
+
+// Implement a program that uses a for loop to iterate through an array of numbers and print only the even numbers.
+
+
+for (let index=0; index<numbers_array.length; index++){
+    if (numbers_array[index]%2 != 0){
+        numbers_array.splice(index,1)
+        --index
+    }
+}
+console.log("All Evin number",numbers_array);
+
+// Implement a program that uses a loop to iterate through an array of numbers and 
+// remove all the even numbers from them and just leave the odd ones
+
+var numbers:number[] = [2,2,3,5,6,8,9,10];
+
+for (let index=0; index<numbers.length; index++){
+    if (numbers[index]%2 == 0){
+        numbers.splice(index,1)
+        --index
+    }
+}
+console.log("All old number",numbers);
